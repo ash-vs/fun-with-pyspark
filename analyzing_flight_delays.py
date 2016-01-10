5aed3a8dd3b810efa741669f46805cff01218aae
Ash Srinivas <ash.v.srinivas@gmail.com>
2016-01-10 14:40:47 -0600

    Added code to produce an RDD of the data transformed into a list of dicts with the headers used as the keys

--- analyzing_flight_delays.py
+++ analyzing_flight_delays.py
@@ -49,5 +49,21 @@ print "Headers:\n{0}".format(header_line_list)
 # a filter that removes any row of data that isn't
 # equal to the header_line we extracted earlier 
 airline_no_header_rdd = airline_no_quote_rdd.filter(lambda row: row != header_line)
-check_list = airline_no_header_rdd.take(4)
+check_list = airline_no_header_rdd.take(2)
 print "Data without headers:\n{0}".format(check_list)
+
+def make_row_dict(row):
+	# split each line's column values by splitting the string by comma 
+	row_line_list = row.split(',')
+	
+	# create a dictionary using the header as keys 
+	# and row items as values by doing the following:
+	# use the zip() function to generate key-value tuples
+	# use the tuples created by zip() to create a dictionary
+	row_dict = dict(zip(header_line_list, row_line_list))
+	return row_dict
+	
+airline_rows_as_dicts_rdd = airline_no_header_rdd.map(make_row_dict)
+check_list = airline_rows_as_dicts_rdd.take(2)
+print "Data in key-value pairs:\n{0}".format(check_list)
+
